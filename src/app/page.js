d42f064fd3b0fe7f85ec2a34d31b162a11084256
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-29 14:54:30 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <main className="text-8xl text-center mt-[10rem] capitalize ">
+      <main className="text-8xl text-center mt-[10rem] capitalize h-[80rem] ">
         big someting
       </main>
     </div>
