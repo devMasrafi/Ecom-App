504139151bb0447402817d4a24b341943e6e5656
km-saifullah <kmsaifullah16@gmail.com>
2024-11-29 18:14:29 +0600

    added cart item design done

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 import { BsCart2 } from "react-icons/bs";
 
 const ShopCart = () => {
   return (
-    <main>
-        <div className='px-2 py-1 cursor-pointer'>
-            <BsCart2 className='text-lg '/>
+    <main className="relative top-0 right-0">
+      <div className="px-2 py-1 cursor-pointer relative">
+        <BsCart2 className="text-2xl" />
+        <div className="absolute -top-2 -right-2 bg-blue-500 text-white h-6 w-6 rounded-full flex items-center justify-center">
+          5
         </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default ShopCart
+export default ShopCart;
