b1bffdd50b675383b14df5c7cccdbade68b1af9e
km-saifullah <kmsaifullah16@gmail.com>
2024-11-29 15:28:04 +0600

    signup page done

--- src/app/(pages)/signup/page.jsx
+++ src/app/(pages)/signup/page.jsx
@@ -1,5 +1,68 @@
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <main>
+      <section className="container mx-auto">
+        <section className="h-[70vh] w-full flex items-center justify-center">
+          <section className="flex flex-col">
+            <div>
+              <h2 className="text-2xl font-semibold mb-4">Register to E-com</h2>
+            </div>
+            <section>
+              <form action="">
+                <section className="space-y-4">
+                  <div>
+                    <label htmlFor="" className="pr-3">
+                      Enter Your Displayname
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter your name"
+                      className="text-base text-black font-normal outline-none border p-3 rounded-lg"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="" className="pr-3">
+                      Enter Your Email
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="Enter your email"
+                      className="text-base text-black font-normal outline-none border p-3 rounded-lg"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="" className="pr-3">
+                      Enter Your phone number
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter your phone number"
+                      className="text-base text-black font-normal outline-none border p-3 rounded-lg"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="" className="pr-3">
+                      Enter Your Password
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="Enter your password"
+                      className="text-base text-black font-normal outline-none border p-3 rounded-lg"
+                    />
+                  </div>
+                  <div>
+                    <button className="px-3 py-2 bg-blue-500 text-base text-white font-normal rounded-lg cursor-pointer hover:bg-blue-700">
+                      Login
+                    </button>
+                  </div>
+                </section>
+              </form>
+            </section>
+          </section>
+        </section>
+      </section>
+    </main>
+  );
 };
 
 export default Signup;
